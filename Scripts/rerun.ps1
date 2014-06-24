23cfda2cb1cf7732f3a78b0ece14220b00029927
adrianvb <github@netzkind.org>
2014-06-24 17:14:48 +0200

    ReRun
    watch a directory and execute a command on change.
    shameless plug from stackexchange

---
+++ Scripts/rerun.ps1
@@ -0,0 +1,19 @@
+﻿Param(
+	[Parameter(Mandatory=$true)] [String] $Path,
+	[Parameter(Mandatory=$true)] [ScriptBlock] $Command
+)
+
+$Watcher = New-Object System.IO.FileSystemWatcher
+$Watcher.Path = $Path
+$Watcher.IncludeSubdirectories = $true
+$Watcher.EnableRaisingEvents = $false
+$Watcher.NotifyFilter = [System.IO.NotifyFilters]::LastWrite -bor [System.IO.NotifyFilters]::FileName
+ 
+while($TRUE){
+	$result = $Watcher.WaitForChanged([System.IO.WatcherChangeTypes]::Changed -bor [System.IO.WatcherChangeTypes]::Renamed -bOr [System.IO.WatcherChangeTypes]::Created, 1000);
+	if($result.TimedOut){
+		continue;
+	}
+	write-host ("Change in {0} " -f $result.Name)
+	& $Command
+}
